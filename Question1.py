31fc21db2569d86c214dc9a2a4d53abd52329649
Sahruday <sahrudaypatti@gmail.com>
2023-02-11 10:57:26 -0500

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -14,3 +14,57 @@ cv2.drawContours(dilation, contours, -1, (0, 255, 0), 7)
 
 cv2.imshow('Contour Image',dilation)
 cv2.waitKey(0)
+
+# =============================================================================
+#  counting number of circles with cv2.HoughCircles()
+# =============================================================================
+
+# finding number of circles in opened_image
+circles = cv2.HoughCircles(dilation,cv2.HOUGH_GRADIENT,1.2,25,param1=30,param2=25,minRadius=0,maxRadius=35)
+
+#gray image to BGR image
+displayImage=cv2.cvtColor(dilation, cv2.COLOR_GRAY2BGR)
+
+disIm=dilation.copy()
+
+# drawing circles on to  the image
+for circle in circles[0]:
+    
+    centerX=int(circle[0])
+    centerY=int(circle[1])
+    r=int(circle[2])
+    cv2.circle(disIm,(centerX,centerY),r,(0,255,0),2)
+    cv2.circle(disIm,(centerX,centerY),2,(0,0,255),5)
+
+cv2.imshow('circles detected by cv2.HoughCircles',disIm)
+# cv2.imwrite('../Results/CirclesDetected_with_HoughCircle.png',disIm)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print('Number of circles detected  by Hough Circles:',len(circles[0]))
+
+# =============================================================================
+# option2-Count of circles through CCA(Connected Component Analysis)
+# =============================================================================
+
+ret,im_labels=cv2.connectedComponents(dilation)
+
+print(f"Number of circles detected by Connected Compont Analysis: {im_labels.max()}")
+
+#visulazing CCA image
+
+minVal=im_labels.min()
+maxVal=im_labels.max()
+
+# normalizing pixel values in between 0 and 255
+normalized_image=255*(im_labels-minVal)/(maxVal-minVal)
+
+normalized_image=np.uint8(normalized_image)
+# gray_normalized_image=
+colormapped_image=cv2.applyColorMap(normalized_image,cv2.COLORMAP_JET)
+
+cv2.imshow('CCA image after colormapping', colormapped_image)
+# cv2.imwrite('../Results/CirclesDetected_with_CCA.png',colormapped_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.destroyAllWindows()
